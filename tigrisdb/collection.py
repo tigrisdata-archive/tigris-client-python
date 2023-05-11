e1f3838accf9ea1f9f5b5079119866c9720208e3
Adil Ansari <adil@tigrisdata.com>
2023-05-11 13:55:29 -0700

    ci: Initializing git workflow

--- tigrisdb/collection.py
+++ tigrisdb/collection.py
@@ -5,7 +5,7 @@ from api.generated.server.v1.api_pb2_grpc import TigrisStub
 from tigrisdb.config import TigrisClientConfig
 from tigrisdb.errors import TigrisException
 from tigrisdb.types import Document
-from tigrisdb.utils import doc_to_bytes, bytes_to_doc
+from tigrisdb.utils import bytes_to_doc, doc_to_bytes
 
 
 class Collection:
@@ -13,7 +13,9 @@ class Collection:
     __config: TigrisClientConfig
     __name: str
 
-    def __init__(self, collection_name: str, client_stub: TigrisStub, config: TigrisClientConfig):
+    def __init__(
+        self, collection_name: str, client_stub: TigrisStub, config: TigrisClientConfig
+    ):
         self.__client = client_stub
         self.__config = config
         self.__name = collection_name
@@ -36,28 +38,28 @@ class Collection:
             project=self.project,
             branch=self.branch,
             collection=self.name,
-            documents=doc_bytes
+            documents=doc_bytes,
         )
         try:
             resp: InsertResponse = self.__client.Insert(req)
-            if resp.status == 'inserted':
+            if resp.status == "inserted":
                 return True
             else:
-                raise TigrisException(f'failed to insert docs: {resp.status}')
+                raise TigrisException(f"failed to insert docs: {resp.status}")
         except grpc.RpcError as e:
-            raise TigrisException(f'failed to insert documents: {e}')
+            raise TigrisException(f"failed to insert documents: {e}")
 
     def find_many(self) -> list[Document]:
         req = ReadRequest(
             project=self.project,
             branch=self.branch,
             collection=self.name,
-            filter=doc_to_bytes({})
+            filter=doc_to_bytes({}),
         )
         try:
             doc_iterator = self.__client.Read(req)
         except grpc.RpcError as e:
-            raise TigrisException(f'failed to read documents: {e}')
+            raise TigrisException(f"failed to read documents: {e}")
 
         docs: list[Document] = []
         for r in doc_iterator:
