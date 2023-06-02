4e9f9dbfd1a3bd23db29c26060d0c8896c283cf5
Adil Ansari <adil@tigrisdata.com>
2023-06-02 12:21:55 -0700

    chore: shorten import paths

---
+++ tigrisdb/__init__.py
@@ -0,0 +1,6 @@
+from client import TigrisClient  # noqa: F401
+from collection import Collection  # noqa: F401
+from database import Database  # noqa: F401
+from search import Search  # noqa: F401
+from search_index import SearchIndex  # noqa: F401
+from vector_store import VectorStore  # noqa: F401
